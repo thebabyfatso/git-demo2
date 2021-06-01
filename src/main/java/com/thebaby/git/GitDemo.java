fe63d1012ce2905146b8211a89573f8cfcf99b4e
thebaby <1578445212@qq.com>
2021-06-01 19:51:35 +0800

    hot-fix first commit

--- src/main/java/com/thebaby/git/GitDemo.java
+++ src/main/java/com/thebaby/git/GitDemo.java
@@ -9,5 +9,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("git demo");
+        System.out.println("hot fix");
     }
 }
