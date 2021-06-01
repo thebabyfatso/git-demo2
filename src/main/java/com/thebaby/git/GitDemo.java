9ae07ce6149e2e8b558ea3aee13fb0179a5fe6e0
thebabyfatso <72146206+thebabyfatso@users.noreply.github.com>
2021-06-01 20:45:28 +0800

    pull test

--- src/main/java/com/thebaby/git/GitDemo.java
+++ src/main/java/com/thebaby/git/GitDemo.java
@@ -13,5 +13,6 @@ public class GitDemo {
         System.out.println("hot fix");
         System.out.println("version 2.0");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
