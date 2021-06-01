d40423c8f0e67493da45ceeb181f0a9995c6bcb4
thebaby <1578445212@qq.com>
2021-06-01 19:33:15 +0800

    first commit

---
+++ src/main/java/com/thebaby/git/GitDemo.java
@@ -0,0 +1,12 @@
+package com.thebaby.git;
+
+/**
+ * @author thebabyfatso
+ * @description
+ * @create 2021-06-01-下午 07:29
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
